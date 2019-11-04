56b24eddbda7068043c0ff62551f2f68a42bdb44
Zahidvillegas <55766078+Zahidvillegas@users.noreply.github.com>
2019-11-04 12:29:41 -0800

    Update GuessingGame.py

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,5 +1,15 @@
+#List
+List1 = []
+list2 = ["Ha Get Rekt Boi"]
+#Adding to the list
+list2.append("Wow!")
+print(list2)
+
+# Choose a random number between 10 and 100
 import random
 randoNumber = random.radint(10,100)
 print(randoNumber)
+
+# Choose a random item from a list
 randolist = random.choice(list2)
 print(randolist)

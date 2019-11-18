4e204d919ff84dbd42e64298e238e2a173de0914
Zahidvillegas <55766078+Zahidvillegas@users.noreply.github.com>
2019-11-18 11:42:37 -0800

    Update GuessingGame.py

--- GuessingGame.py
+++ GuessingGame.py
@@ -4,7 +4,7 @@ rnum = random.randint (1000, 9999)
 print (rnum)
 #guesses vriable
 guesses = 5
-usersuccess = ["-,-,-,-"]
+usersuccess = ["-", "-", "-", "-"]
 
 #makes random nmber list so that it can be compared to the number that is guessed
 strnum = str (rnum)
@@ -27,20 +27,20 @@ while guesses != 0:
 
 #if it isnt right then it checks each index to see if they match and shows the player what numbrs match
     else:
-        for i in rnumlist:
-            for j in guesslist:
+        for i in guesslist:
+            for j in rnumlist:
                 if i == j :
-                    rnumindex = rnumlist.index(i)
-                    guessindex = guesslist.index(j)
+                    rnumindex = rnumlist.index(j)
+                    guessindex = guesslist.index(i)
                     print(rnumindex, guessindex)
                     if rnumindex == guessindex:
                         print ("One of Your numbers matches")
                         usersuccess.pop(rnumindex)
+                        usersuccess.insert(rnumindex, i)
 
 
-                        print (usersuccess)
-                    else:
-                        pass
+
+                    print (usersuccess)
                 else:
                     print ("-")
     guesslist.clear ()
